b018ea0704f150d6182035efde2a2c35f3d4a8b8
Rutvik <rutvikraut567@gmail.com>
2024-09-22 13:44:10 +0530

    MongoDB Connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MONGODB connected to ${connectionInstance.connection.host} `);
+        console.log(`\n MONGODB connected to ${connectionInstance} `);
+    }catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB
